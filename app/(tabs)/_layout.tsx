21a8af08e3833d5e5a3ed73caacebaaa22da0242
Elisio Augusto <elisiouiux@gmail.com>
2026-04-28 23:54:00 +0100

    add icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,18 +1,44 @@
+import { tabs } from "@/constants/data";
+import { clsx } from "clsx";
 import { Tabs } from "expo-router";
 import React from "react";
+import { Image, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function TabLayout() {
+  const insets = useSafeAreaInsets();
+
+  const TabIcon = ({ focused, icon }: TabIconProps) => {
+    return (
+      <View className="icon-tabs">
+        <View className={clsx("tabs-pill", focused && "tabs-active")}>
+          <Image source={icon} resizeMode="contain" className="tabs-glyph" />
+        </View>
+      </View>
+    );
+  };
+
   return (
-    <Tabs>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="subscriptions" options={{ title: "Subscriptions" }} />
-      <Tabs.Screen
-        name="insights"
-        options={{
-          title: "Insights",
-        }}
-      />
-      <Tabs.Screen name="settings" options={{ title: "Settings" }} />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarShowLabel: false,
+        tabBarStyle: { position: "absolute", bottom: Math.max(insets.bottom) },
+      }}
+    >
+      {tabs.map((tab) => (
+        <Tabs.Screen
+          name={tab.name}
+          options={{
+            title: tab.title,
+            tabBarIcon: ({ focused }) => (
+              <TabIcon focused={focused} icon={tab.icon} />
+            ),
+          }}
+          key={tab.name}
+        />
+      ))}
+
       <Tabs.Screen
         name="subscriptions/[id]"
         options={{
--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -3,8 +3,8 @@ import { Text, View } from "react-native";
 
 export default function Settings() {
   return (
-    <View>
-      <Text>Settings</Text>
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">Settings</Text>
     </View>
   );
 }
